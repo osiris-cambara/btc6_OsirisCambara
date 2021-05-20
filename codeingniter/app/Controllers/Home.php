be6e28e70195a694d6341f0ee6a70b136357fe14
osiris-cambara <83963692+osiris-cambara@users.noreply.github.com>
2021-05-20 09:37:27 -0500

    Update Home.php

--- codeingniter/app/Controllers/Home.php
+++ codeingniter/app/Controllers/Home.php
@@ -5,7 +5,10 @@ namespace App\Controllers;
 class Home extends BaseController
 {
 	public function index()
-	{
+	{	
+		  $data['mydata1']="testing1";
+                  $data['mydata2']="testing2";
+		return view('form',$data);
 		return view('welcome_message');
 	}
 }
