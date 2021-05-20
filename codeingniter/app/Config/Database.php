de2996fa2d05975c8529e8c07c9434770a840bae
osiris-cambara <83963692+osiris-cambara@users.noreply.github.com>
2021-05-20 10:18:56 -0500

    Update Database.php

--- codeingniter/app/Config/Database.php
+++ codeingniter/app/Config/Database.php
@@ -75,9 +75,7 @@ class Database extends Config
 		'failover' => [],
 		'port'     => 3306,
 	];
-	  $data['mydata1']="nombre_usuario"
-	  $data['mydata2']="pasword"  
-	return view ('form'.$data);
+	 
 	//--------------------------------------------------------------------
 
 	public function __construct()
