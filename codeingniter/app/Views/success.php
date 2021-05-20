a7761dda90f8b5612a69d1940e48cf069103ad1b
osiris-cambara <83963692+osiris-cambara@users.noreply.github.com>
2021-05-20 10:26:33 -0500

    Update success.php

--- codeingniter/app/Views/success.php
+++ codeingniter/app/Views/success.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-        <br><br> <h1 style="color:#0fad00">user_name</h2>
+        <br><br> <h1 style="color:#0fad00">user_name</h>
         <br><br> <h2 style="color:#0fad00">password</h2>
         </div>
     </div>
