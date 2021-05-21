37344a1f9569eca3787a57ccca59eb0d13c0216e
osiris-cambara <83963692+osiris-cambara@users.noreply.github.com>
2021-05-21 14:31:57 -0500

    Update LoginController.php

--- codeingniter/app/Controllers/LoginController.php
+++ codeingniter/app/Controllers/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
         $rows = $this->login->countAllResults();
         $session = session();          
         if($rows==1){
-            return view('success',data);
+            return view('success',$data);
        
         }else{
             $session->setFlashdata('msg', 'Invalid User');
